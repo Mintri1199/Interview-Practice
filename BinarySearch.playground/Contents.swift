222360b060c2898c67325f16d59c3390c5f4dba8
Jackson Ho <mintri1199@gmail.com>
2019-07-03 00:26:38 -0700

    [Added] Added the fourth exercism problem

---
+++ BinarySearch.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Implement Binary Search for an array of integers
+
+func binarySearch(array: [Int], target: Int) -> Int? {
+  var startingPoint = 0
+  var endPoint = array.count - 1
+  
+  while endPoint >= startingPoint {
+    // The middle index of the array if the average of two end points
+    let middleIndex = abs((startingPoint + endPoint) / 2)
+    
+    if array[middleIndex] == target{
+      return middleIndex
+    } else if array[middleIndex] > target {
+      endPoint = middleIndex - 1
+    } else {
+      startingPoint = middleIndex + 1
+    }
+  }
+  return nil
+}
